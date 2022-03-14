99fa1d5dc1658a976d469612fb9c739a196c4f56
mdjibo <hafizouadamou1@gmail.com>
2022-03-14 15:13:00 -0700

    Print last digit and compare

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 		}
 		else if (lastdigit == 0)
 		{
-			printf("The last digit of %d is %d and is 0\n", n, lastdigit);
+			printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 		}
 		else
 		{
