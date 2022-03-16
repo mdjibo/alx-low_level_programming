5921bd80d01c2243b66644384eda5a6b65dbe756
mdjibo <hafizouadamou1@gmail.com>
2022-03-16 04:55:15 -0700

    Correction

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
+ *main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-	printf("Holberton School\n");
 	    return (0);
-
 }
 
 
--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include"main.h"
 #include<stdio.h>
 /**
- * print string without using printf
+ *Print string without using printf
  *Using our own header
  */
 int main(void)
